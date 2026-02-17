3f687aa249d7f9d3e1c387857650ee6faaf8df2e
falahadeel-ctrl <falahadeel@gmail.com>
2026-02-16 22:16:56 -0500

    erb

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 //imports
+import express from "expess";
+import db from './db/conn';
+
+const app = express();
+const PORT = 3000;
+
+app.json();
+
 //middleware
-//
+app.use(app.json()); //middleware that runs before routes.
+app.use(logReq);
+
+//routes
+
 //errorhandleing
 //handler
