366fd8883d52376277e7acef412ae012ed5d5826
falahadeel-ctrl <falahadeel@gmail.com>
2026-02-17 19:40:15 -0500

    new

--- routes/Animals.js
+++ routes/Animals.js
@@ -3,4 +3,23 @@ import conn from `../db/conn.js`;
 
 const router = express.Router();
 
-router.get
+//getting all the animals
+router.get(`/`, async function(req,res){
+    try{
+const animals = db.collection(`animals`).find([]).toArray();
+res.json(animals);
+    }catch(err){
+res.status(500).json({err.message});
+    }
+});
+
+//post new animal
+router.post(`/`,async (req,res)=>{
+    let newAnimal = req.body;
+    let collection = await db.collection(`animals`);
+    let result = await collection.insertOne(`newAnimal`);
+   res.json(result);
+});
+
+//updating animals
+router.patch(``)
